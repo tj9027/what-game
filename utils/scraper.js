d770d531ff7207feae1a807d5ca4a1016a58f834
joseph <jphctolentino@gmail.com>
2020-05-13 11:04:38 +0100

    add test

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,23 +1,24 @@
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
 async function scraper(browser, url, type) {
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 800 });
   await page.goto(url);
+  //add this test
 
   try {
-    await page.waitForSelector('body', { timeout: 5000 });
+    await page.waitForSelector("body", { timeout: 5000 });
   } catch (e) {
     return results;
   }
 
   const results = {};
-  if (type === 'youtube') {
-    await page.waitForSelector('div#contents', {
+  if (type === "youtube") {
+    await page.waitForSelector("div#contents", {
       timeout: 10000,
     });
-  } else if (type === 'twitch') {
-    await page.waitForSelector('div.channel-header', {
+  } else if (type === "twitch") {
+    await page.waitForSelector("div.channel-header", {
       timeout: 10000,
     });
   }
@@ -29,26 +30,26 @@ async function scraper(browser, url, type) {
 
 function parse(html, type) {
   const $ = cheerio.load(html);
-  if (type === 'youtube') {
+  if (type === "youtube") {
     return {
-      gameTitle: $('div#title', 'div#text-container').html(),
-      gameLink: 'https://www.youtube.com' + $('a', 'div#contents').attr('href'),
+      gameTitle: $("div#title", "div#text-container").html(),
+      gameLink: "https://www.youtube.com" + $("a", "div#contents").attr("href"),
     };
-  } else if (type === 'twitch') {
+  } else if (type === "twitch") {
     return {
       status:
         $(
-          '.tw-channel-status-text-indicator',
+          ".tw-channel-status-text-indicator",
           // '.channel-header-user-tab__user-content',
-          'div.channel-header'
-        ).text() === 'Live'
-          ? 'online'
-          : 'offline',
+          "div.channel-header"
+        ).text() === "Live"
+          ? "online"
+          : "offline",
       game: $(
-        'img',
+        "img",
         // 'a.tw-interactive',
-        '.channel-info-bar__info-container'
-      ).attr('alt'),
+        ".channel-info-bar__info-container"
+      ).attr("alt"),
     };
   }
 }
